09bcca57b77de589190d01de77229073ca0a45ae
Xyven1 <maritimus987@gmail.com>
2022-07-08 22:21:14 -0400

    working version 1.0

--- main.go
+++ main.go
@@ -5,17 +5,16 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 )
 
 func main() {
-	// Hello world, the web server
-
 	args := os.Args[1:]
 	if len(args) != 2 {
 		log.Fatal("Usage: ./main <file> <port>")
 	}
 
-	f, err := os.Open(args[0])
+	f, err := os.OpenFile(args[0], os.O_RDWR, 0666);
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
@@ -29,7 +28,6 @@ func main() {
 	}
 	defer l.Close()
 
-	// create list of conns
 	conns := make(map[net.Conn]bool)
 	go ReadFileAndSendToAll(f, conns)
 	for {
@@ -45,23 +43,31 @@ func main() {
 
 func HandleConnection(c net.Conn, f *os.File, conns map[net.Conn]bool) {
 	defer c.Close()
-  fmt.Println("Handling connection")
   b := make([]byte, 1024)
-	for s, err := c.Read(b); s > 0 && err == nil; {
-    f.Write(b[:s])
-	}
+  for {
+    n, err := c.Read(b)
+    if err != nil {
+      break
+    }
+    f.Write(b[:n])
+  }
   delete(conns, c)
 }
 
 func ReadFileAndSendToAll(f *os.File, conns map[net.Conn]bool) {
-  fmt.Println("Reading file")
+  fmt.Println("Loading file")
 	b := make([]byte, 1024)
-	for s, err := f.Read(b); s > 0 && err == nil; {
-    fmt.Println("Sending to all: ", s)
-		for conn := range conns {
-			if conns[conn] {
-				conn.Write(b[:s])
-			}
-		}
+  c:=0
+	for {
+    n, err := f.Read(b)
+    if err != nil {
+      fmt.Println("Error reading from file:", err)
+      continue
+    }
+    fmt.Printf("\r%s\tNum Clients: %d\tNum Mavlink Packets: %d", time.Now().Format("2006-01-02 15:04:05"), len(conns), c)
+    c++
+    for conn := range conns {
+      conn.Write(b[:n])
+    }
 	}
 }
