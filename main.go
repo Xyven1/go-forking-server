f7499ea35adfc295459013f34f97e995641ed56d
Xyven1 <maritimus987@gmail.com>
2022-10-17 21:06:03 -0400

    refactored using struct to keep track of data

--- main.go
+++ main.go
@@ -14,13 +14,20 @@ import (
 )
 
 type WrapSync[T any] struct {
-	v  T
-	mu sync.Mutex
+	v  T          // value of type T
+	mu sync.Mutex // mutex to protect v
+}
+
+type ForkingServer struct {
+	serial WrapSync[serial.Port]  // the serial port we are listnening on
+	udp    WrapSync[*net.UDPConn] // the udp port we are using to broadcast
+	conns  map[net.Conn]bool      // a map of all current tcp connections
+	addrs  map[string]int         // a list of the addresses of all current tcp connections
+	port   string                 // name of the serial port to connect to
 }
 
 func main() {
 	args := os.Args[1:]
-	osName := runtime.GOOS
 
 	if len(args) != 2 {
 		log.Fatal(`Usage: ./main <file> <port>
@@ -28,32 +35,11 @@ func main() {
 <port> - The port to listen on`)
 	}
 
-	var sn string
-	if osName == "linux" {
-		fs, err := filepath.Glob(args[0])
-		if err != nil {
-			log.Fatal(err)
-		}
-		if len(fs) == 0 {
-			log.Fatal("No files found")
-		}
-		sn = fs[0]
-	} else if osName == "windows" {
-		if args[0][:3] != "COM" {
-			log.Fatal("Windows port must start with COM")
-		}
-		sn = args[0]
-	} else {
-		log.Fatal("Unsupported OS")
-	}
-
-	mode := &serial.Mode{}
-	s, err := serial.Open(sn, mode)
-	if err != nil {
-		log.Fatal(err)
-	}
-	serial := WrapSync[serial.Port]{v: s}
-	defer s.Close()
+	server := ForkingServer{}
+	server.port = args[0]
+	server.serial.mu.Lock()
+	server.startSerial()
+	server.serial.mu.Unlock()
 
 	l, err := net.Listen("tcp", ":"+args[1])
 	if err != nil {
@@ -61,64 +47,90 @@ func main() {
 	}
 	defer l.Close()
 
-	u, err := net.ListenUDP("udp", &net.UDPAddr{IP: nil, Port: 14550})
+	server.udp.v, err = net.ListenUDP("udp", &net.UDPAddr{IP: nil, Port: 14550})
 	if err != nil {
 		log.Fatal(err)
 	}
-	udp := WrapSync[*net.UDPConn]{}
-	udp.v = u
 
-	conns := make(map[net.Conn]bool)
-	addrs := make(map[string]int)
-	go readFileAndSendToAll(&serial, &udp, conns)
-	go udpReceive(&udp, &serial, addrs)
+	go server.readFileAndSendToAll()
+	go server.udpReceive()
 	for {
 		conn, err := l.Accept()
 		if err != nil {
 			log.Fatal(err)
 			continue
 		}
-		go handleConnection(conn, &udp, &serial, conns, addrs)
+		go server.handleConnection(conn)
 	}
 }
 
-func udpReceive(u *WrapSync[*net.UDPConn], s *WrapSync[serial.Port], addrs map[string]int) {
+// call with s.mu locked
+func (server *ForkingServer) startSerial() {
+	osName := runtime.GOOS
+	mode := &serial.Mode{}
+	var sn string
+	switch osName {
+	case "linux":
+		fs, err := filepath.Glob(server.port)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(fs) == 0 {
+			log.Fatal("No files found")
+		}
+		sn = fs[0]
+	case "windows":
+		if server.port[:3] != "COM" {
+			log.Fatal("Windows port must start with COM")
+		}
+		sn = server.port
+	default:
+		log.Fatal("Unsupported OS")
+	}
+	var err error
+	server.serial.v, err = serial.Open(sn, mode)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (server *ForkingServer) udpReceive() {
 	for {
 		buf := make([]byte, 1024)
-		n, addr, err := u.v.ReadFromUDP(buf)
+		n, addr, err := server.udp.v.ReadFromUDP(buf)
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-		if addrs[addr.IP.String()] > 0 && n > 0 {
-			s.mu.Lock()
-			s.v.Write(buf[:n])
-			s.mu.Unlock()
+		if server.addrs[addr.IP.String()] > 0 && n > 0 {
+			server.serial.mu.Lock()
+			server.serial.v.Write(buf[:n])
+			server.serial.mu.Unlock()
 		}
 	}
 }
 
-func handleConnection(c net.Conn, u *WrapSync[*net.UDPConn], s *WrapSync[serial.Port], conns map[net.Conn]bool, addrs map[string]int) {
+func (server *ForkingServer) handleConnection(c net.Conn) {
 	defer c.Close()
 	a, err := net.ResolveTCPAddr("tcp", c.RemoteAddr().String())
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	addrs[a.IP.String()]++
-	conns[c] = true
+	server.addrs[a.IP.String()]++
+	server.conns[c] = true
 	b := make([]byte, 1024)
 	for {
 		n, err := c.Read(b)
 		if err != nil {
 			break
 		}
-		s.mu.Lock()
-		s.v.Write(b[:n])
-		s.mu.Unlock()
+		server.serial.mu.Lock()
+		server.serial.v.Write(b[:n])
+		server.serial.mu.Unlock()
 	}
-	addrs[a.IP.String()]--
-	delete(conns, c)
+	server.addrs[a.IP.String()]--
+	delete(server.conns, c)
 }
 
 func udpBroadcast(u *WrapSync[*net.UDPConn], r net.Addr, b []byte) {
@@ -136,7 +148,7 @@ func udpBroadcast(u *WrapSync[*net.UDPConn], r net.Addr, b []byte) {
 	}
 }
 
-func readFileAndSendToAll(s *WrapSync[serial.Port], u *WrapSync[*net.UDPConn], conns map[net.Conn]bool) {
+func (server *ForkingServer) readFileAndSendToAll() {
 	fmt.Println("Loading file")
 	b := make([]byte, 1024)
 	c := 0
@@ -147,23 +159,27 @@ func readFileAndSendToAll(s *WrapSync[serial.Port], u *WrapSync[*net.UDPConn], c
 	}
 
 	for {
-		n, err := s.v.Read(b)
+		server.serial.mu.Lock()
+		n, err := server.serial.v.Read(b)
 		if err != nil {
 			fmt.Println(err)
-			continue
+			server.serial.v.Close()
+			server.startSerial()
 		}
+		server.serial.mu.Unlock()
+
 		if (o.Mode() & os.ModeCharDevice) == os.ModeCharDevice {
-			fmt.Printf("\r%s\tNum Clients: %d\tNum Mavlink Packets: %d  ", time.Now().Format("2006/01/02 15:04:05"), len(conns), c)
+			fmt.Printf("\r%s\tNum Clients: %d\tNum Mavlink Packets: %d  ", time.Now().Format("2006/01/02 15:04:05"), len(server.conns), c)
 		} else {
 			if time.Now().Sub(t) > time.Second*10 {
-				fmt.Printf("%s\tNum Clients: %d\tNum Mavlink Packets: %d\n", time.Now().Format("2006/01/02 15:04:05"), len(conns), c)
+				fmt.Printf("%s\tNum Clients: %d\tNum Mavlink Packets: %d\n", time.Now().Format("2006/01/02 15:04:05"), len(server.conns), c)
 				t = time.Now()
 			}
 		}
 		c++
-		for conn := range conns {
+		for conn := range server.conns {
 			conn.Write(b[:n])
-			udpBroadcast(u, conn.RemoteAddr(), b[:n])
+			udpBroadcast(&server.udp, conn.RemoteAddr(), b[:n])
 		}
 	}
 }
